32683d966a1a8618add3e38b4feaf3c7debb9721
Fenrir12 <sotistechno@gmail.com>
2025-07-25 07:44:20 -0400

    feat: remove https from src, will be done at deployment
    Ref: PH-17

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 const swaggerUi = require('swagger-ui-express')
 const swaggerSpec = require('./swagger/swagger')
-const https = require('https')
-const fs = require('fs')
 
 const authRoutes = require('./routes/authRoutes')
 const pinsRoutes = require('./routes/pinsRoutes')
@@ -23,11 +21,7 @@ app.use('/api/parks', parkRoutes)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 const PORT = process.env.PORT
-const httpsOptions = {
-  key: fs.readFileSync('./secrets/server.key'),
-  cert: fs.readFileSync('./secrets/server.crt'),
-}
 
-https.createServer(httpsOptions, app).listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`🚀 HTTPS Server running on https://sam-mac.local:${PORT}`)
 })
