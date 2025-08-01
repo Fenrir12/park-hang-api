51ddae49b2072d2e8e94dee61075d8e0585214cb
Fenrir12 <sotistechno@gmail.com>
2025-08-01 15:36:16 -0400

    feat: add ttl to models

---
+++ src/config/ttl.js
@@ -0,0 +1,4 @@
+export const TTL_SECONDS = {
+  CHECKIN_EXPIRATION: 60 * 60 * 8, // 2 hours (example)
+  HANGOUT_EXPIRATION: 60 * 60 * 4, // 4 hours
+}
--- src/infrastructure/db/mongo/models/CheckInModel.js
+++ src/infrastructure/db/mongo/models/CheckInModel.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
+const { TTL_SECONDS } = require('../../../../config/ttl')
 
-const checkInSchema = new mongoose.Schema({
-  checkInId: { type: String, required: true },
-  userId: { type: String },
-  isAnonymous: { type: Boolean, default: true },
-  currentPark: { type: Object },
-  timestamp: { type: Date, default: Date.now },
-})
+const checkInSchema = new mongoose.Schema(
+  {
+    checkInId: { type: String, required: true },
+    userId: { type: String },
+    isAnonymous: { type: Boolean, default: true },
+    currentPark: { type: Object },
+    timestamp: { type: Date, default: Date.now },
+  },
+  { timestamps: true },
+)
+checkInSchema.index(
+  { timestamp: 1 },
+  { expireAfterSeconds: TTL_SECONDS.CHECKIN_EXPIRATION },
+)
 
 module.exports = mongoose.model('CheckIn', checkInSchema)
